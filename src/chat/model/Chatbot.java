0f21705c5a4b2d37d9882629a337dff5d9c82d02
Sparticus403 <twil8980@740s-pl-332.local>
2016-10-14 08:41:52 -0600

    Created repo for the ChatBot projet

---
+++ src/chat/model/Chatbot.java
@@ -0,0 +1,11 @@
+package chat.model;
+
+import java.util.ArrayList;
+
+public class Chatbot
+{
+	private ArrayList<String> memesList;
+	private ArrayList<String> politicalTopicList;
+	private String userName;
+	private String content;
+}
