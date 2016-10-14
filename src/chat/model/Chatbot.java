a15a39799c213b391cfd46e663c20d7cd0eb8137
Sparticus403 <twil8980@740s-pl-332.local>
2016-10-14 08:53:42 -0600

    finished runner class

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,11 +1,65 @@
 package chat.model;
 
 import java.util.ArrayList;
-
+y
 public class Chatbot
 {
 	private ArrayList<String> memesList;
 	private ArrayList<String> politicalTopicList;
 	private String userName;
 	private String content;
+	
+	public Chatbot(String userName)
+	{
+		
+	}
+	private void buildMemesList()
+	{
+		
+	}
+	private void buildPoliticalTopicsList()
+	{
+		
+	}
+	
+	public boolean lengthChecker(String currentInput)
+	{
+		return false;
+	}
+	
+	public boolean contentChecker(String curreInput)
+	{
+		return false;
+	}
+	public boolean poiticalTopicChecker(String currentInput)
+	{
+		return false;
+	}
+	public boolean memeChecker(String currentInput)
+	{
+		return false;
+	}
+	public String getUserName()
+	{
+		return null;
+	}
+	public String getContent()
+	{
+		return null;
+	}
+	/**  * Getter method for the memesList object.  
+	 * * @return The reference to the meme list.  
+	 * */ 
+	public ArrayList<String> getMemesList() 
+	{
+		return null;
+	}
+		/**  * Getter method for the politicalTopicList object.  * @return The reference to the political topic list.  */ 
+	public ArrayList<String> getPoliticalTopicList() 
+	{
+		return null;
+	}
+		/**  * Updates the content area for this Chatbot instance.  * @param content The updated value for the content area.  */ 
+	public void setContent(String content) 
+	{
 }
